58b85c01e60c47a16c7cb1e45d2bdd54c3a08817
absornurul <absornurul97@gmail.com>
2025-12-26 08:46:55 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-// Fungsi buka undangan
 function openInvite() {
-  const hero = document.querySelector('.hero');
+  const hero = document.getElementById('hero');
   const content = document.getElementById('content');
 
   if (hero && content) {
@@ -12,10 +11,12 @@ function openInvite() {
 // Ambil nama tamu dari URL (?to=Nama)
 document.addEventListener('DOMContentLoaded', function () {
   const params = new URLSearchParams(window.location.search);
-  const guestName = params.get('to');
+  const guest = params.get('to');
 
-  const guestEl = document.getElementById('guest');
-  if (guestName && guestEl) {
-    guestEl.innerText = guestName.replace(/\+/g, ' ');
+  if (guest) {
+    const guestEl = document.getElementById('guest');
+    if (guestEl) {
+      guestEl.innerText = guest.replace(/\+/g, ' ');
+    }
   }
 });
